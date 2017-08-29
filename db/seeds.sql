2a24ee4ba0ba200610628e8156d26b85a48242dc
Karin Hermann <karinclimber@gmail.com>
2017-08-29 10:46:06 -0400

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cheeseburger', true, '2017-08-28');
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Waffle Burger', false, '2017-08-28');
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Burger', false, '2017-08-28');
